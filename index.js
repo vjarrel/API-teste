628ebd84052131845b11b2f48463723f751eb9e9
Vitor Jarrel <vjarrel123@gmail.com>
2022-09-05 18:22:15 -0300

    divisao

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/soma', (req, res)=> {
+app.get('/divisao', (req, res)=> {
     const query = req.query
     
-    const subtracao = Number(query.n1) - Number(query.n2)
-    res.send(`O resultado é ${subtracao}`)
+    const divisao = Number(query.n1)/Number(query.n2)
+    res.send(`O resultado é ${divisao}`)
 })
 app.listen(3333,()=> console.log('servidor aberto!'))
