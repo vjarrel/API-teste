8a9f9fb44f0e73fa69999f58b9c0e72c466f7b54
Vitor Jarrel <vjarrel123@gmail.com>
2022-09-05 18:25:00 -0300

    multiplicacao

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/divisao', (req, res)=> {
+app.get('/multiplicacao', (req, res)=> {
     const query = req.query
     
-    const divisao = Number(query.n1)/Number(query.n2)
-    res.send(`O resultado é ${divisao}`)
+    const multiplicacao = Number(query.n1)*Number(query.n2)
+    res.send(`O resultado é ${multiplicacao}`)
 })
 app.listen(3333,()=> console.log('servidor aberto!'))
