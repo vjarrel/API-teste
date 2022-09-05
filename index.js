7ff29c1efe61b774a270a12e70337ef19041e13a
Vitor Jarrel <vjarrel123@gmail.com>
2022-09-05 18:10:07 -0300

    subtracao

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 app.get('/soma', (req, res)=> {
-    const soma = 5+5
-    res.send(`O resultado é ${soma}`)
+    const query = req.query
+    
+    const subtracao = Number(query.n1) - Number(query.n2)
+    res.send(`O resultado é ${subtracao}`)
 })
 app.listen(3333,()=> console.log('servidor aberto!'))
